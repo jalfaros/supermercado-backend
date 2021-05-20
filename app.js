dfe2a247ee549cbfb4a17bc1b53e7b1f064d8ce5
Jose Ignacio Alfaro <ignacioalfaro99@gmail.com>
2021-05-20 00:51:10 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+      );    
+    res.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT");
+    next();
+});
+
+
+app.get('/', ( req, res ) => {
+    res.send('Backend is working...')
+});
+
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const http = require('http');
+
+
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST || 'localhost';
+
+
+const server = http.createServer(app);
+
+
+server.listen( port, () => {
+    console.log(`Servidor corriendo en http://${hostname}:` + server.address().port )
+})
