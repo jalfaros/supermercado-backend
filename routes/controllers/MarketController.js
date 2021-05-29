0674c869abaebfd84c978c6286614ac64089acf3
Warner Hurtado <warner3898@gmail.com>
2021-05-29 01:19:11 -0600

    listo

--- routes/controllers/MarketController.js
+++ routes/controllers/MarketController.js
@@ -59,5 +59,39 @@ router.post('/newMarket', async (req, res) => {
     }
 })
 
+// {
+//     const userId = req.query.uid;
+//     var db = firebase.firestore();
+//     const userMarkets = [];
+
+//     marketsRef = await db.collection('markets').get()
+
+//     counter = 0;
+//     await marketsRef.forEach( snapshot => {
+//         if( snapshot.data()['createdBy'] === userId ){
+//             userMarkets.push( snapshot.data() );
+//             userMarkets[counter]['documentId'] = snapshot.id;
+//             counter ++;
+//         }
+//     });
+    
+//     res.status(httpStatus.OK).json({ data: userMarkets, success: true })
+
+// } 
+router.get('/getMarketForId', async(req, res) => {
+    try{
+        const idMarket = req.query.idMarket;
+        var db = firebase.firestore();
+        console.log(idMarket + ' el idMarket');
+        const marketRef = db.collection('markets').doc(idMarket);
+        const doc = await marketRef.get();
+
+        res.status(httpStatus.OK).json({ data: doc.data(), success: true });
+
+    }catch(error){
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({error , success: false });
+    }
+})
+
 
 module.exports = router;
